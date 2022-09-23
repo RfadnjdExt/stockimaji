b2e8948b729f36e4f4048ba10261e65614d19010
RfadnjdExt <raficanggih@gmail.com>
2022-09-23 10:29:03 +0700

    first commit

---
+++ src/lib/nav.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+        <div>
+            StockImagi
+        </div>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+        </div>
+    </div>
+</nav>
+
+<style>
+    .nav-container {
+        @apply h-20 bg-indigo-500 text-white font-bold text-2xl flex items-center px-5 shadow-lg;
+    }
+
+    .nav-container > div:nth-child(2) {
+        @apply ml-auto text-xl
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+    import Nav from "../lib/nav.svelte";
+</script>
+
+<Nav />
+
+<main></main>
